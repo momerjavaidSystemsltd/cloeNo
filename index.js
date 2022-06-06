7b4861ae1f028e5f62cd31756b01a377d7f09dfd
momerjavaidSystemsltd <105357097+momerjavaidSystemsltd@users.noreply.github.com>
2022-06-05 20:11:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('New hello there!');
+  res.send(' a simple task which took me 3 week's shows the level of my incompetence ');
 });
 
 app.listen(3000, function () {
